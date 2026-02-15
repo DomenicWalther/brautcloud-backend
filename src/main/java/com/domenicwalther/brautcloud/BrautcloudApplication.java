f90751340efbf5ec094f3503f9995910bb93bf5b
DomenicWalther <domenicwalthercreatiiv@gmail.com>
2026-02-15 01:47:27 +0100

    initial commit

---
+++ src/main/java/com/domenicwalther/brautcloud/BrautcloudApplication.java
@@ -0,0 +1,13 @@
+package com.domenicwalther.brautcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrautcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrautcloudApplication.class, args);
+	}
+
+}
