36f4bb0465c2c38c121299215192188a23435bf1
DomenicWalther <domenicwalthercreatiiv@gmail.com>
2026-02-17 00:23:40 +0100

    feat(user): Add very basic user model and UserController

--- src/main/java/com/domenicwalther/brautcloud/User.java
+++ src/main/java/com/domenicwalther/brautcloud/User.java
@@ -1,4 +1,21 @@
 package com.domenicwalther.brautcloud;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDateTime;
+
+@Data
 public class User {
+
+    @Id
+    private String id;
+
+    private String lastName;
+    private String firstNameCoupleOne;
+    private String firstNameCoupleTwo;
+    private LocalDateTime created_at;
+    private String email;
+    private boolean email_verified;
+    private String password;
 }
--- src/main/java/com/domenicwalther/brautcloud/UserController.java
+++ src/main/java/com/domenicwalther/brautcloud/UserController.java
@@ -1,4 +1,26 @@
 package com.domenicwalther.brautcloud;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
 public class UserController {
+
+    Map<String, User> users = new HashMap<>();
+
+    @GetMapping("/users")
+    public Collection<User> getUsers(){
+        return users.values();
+    }
+
+    @PostMapping("/users")
+    public void addUser(@RequestBody User user){
+        users.put(user.getId(), user);
+    }
 }
