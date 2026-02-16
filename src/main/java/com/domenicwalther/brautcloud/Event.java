9d6d421cfc45139f10b412933561845282dd372a
DomenicWalther <domenicwalthercreatiiv@gmail.com>
2026-02-17 00:37:05 +0100

    feat(event): Add basic Model and EventController implementation

--- src/main/java/com/domenicwalther/brautcloud/Event.java
+++ src/main/java/com/domenicwalther/brautcloud/Event.java
@@ -1,4 +1,23 @@
 package com.domenicwalther.brautcloud;
 
+import jakarta.persistence.Column;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDateTime;
+
+@Data
 public class Event {
+
+    @Id
+    private String id;
+    private String user_id;
+    private String event_name;
+    @Column(name = "location")
+    private String event_location;
+    @Column(name = "date")
+    private LocalDateTime event_date;
+    private String password;
+    private String qr_code;
+    private LocalDateTime created_at;
 }
--- src/main/java/com/domenicwalther/brautcloud/EventController.java
+++ src/main/java/com/domenicwalther/brautcloud/EventController.java
@@ -1,4 +1,28 @@
 package com.domenicwalther.brautcloud;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
 public class EventController {
+
+    Map<String, Event> events = new HashMap<>();
+
+
+    @GetMapping("/events")
+    public Collection<Event> getEvents(){
+        return events.values();
+    }
+
+    @PostMapping("/events")
+    public void addEvent(@RequestBody Event event){
+        events.put(event.getId(), event);
+    }
+
 }
