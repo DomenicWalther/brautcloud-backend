24d9112b51191252887236d013d07f3b41160352
DomenicWalther <domenicwalthercreatiiv@gmail.com>
2026-02-17 19:49:39 +0100

    refactor(repository): Change from CrudRepository to JpaRepository

--- src/main/java/com/domenicwalther/brautcloud/repository/UserRepository.java
+++ src/main/java/com/domenicwalther/brautcloud/repository/UserRepository.java
@@ -1,7 +1,7 @@
 package com.domenicwalther.brautcloud.repository;
 
 import com.domenicwalther.brautcloud.model.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends CrudRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
