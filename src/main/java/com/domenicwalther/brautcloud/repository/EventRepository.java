2dad6e035ccef32fc4411a80fc44d8774d449850
DomenicWalther <domenicwalthercreatiiv@gmail.com>
2026-02-17 19:50:06 +0100

    feat(repository): add EventRepository

---
+++ src/main/java/com/domenicwalther/brautcloud/repository/EventRepository.java
@@ -0,0 +1,7 @@
+package com.domenicwalther.brautcloud.repository;
+
+import com.domenicwalther.brautcloud.model.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventRepository extends JpaRepository<Event, Long> {
+}
