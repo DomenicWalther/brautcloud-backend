57bbd28d557dc4f1ba3d8a57772582276b336cc6
DomenicWalther <domenicwalthercreatiiv@gmail.com>
2026-02-17 19:51:21 +0100

    feat(event): Add ability to createEvents, use new EventResponse and EventRequest

--- src/main/java/com/domenicwalther/brautcloud/service/EventService.java
+++ src/main/java/com/domenicwalther/brautcloud/service/EventService.java
@@ -1,22 +1,41 @@
 package com.domenicwalther.brautcloud.service;
 
+import com.domenicwalther.brautcloud.dto.EventRequest;
+import com.domenicwalther.brautcloud.dto.EventResponse;
 import com.domenicwalther.brautcloud.model.Event;
+import com.domenicwalther.brautcloud.model.User;
+import com.domenicwalther.brautcloud.repository.EventRepository;
+import com.domenicwalther.brautcloud.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
+
 
 @Service
 public class EventService {
 
-    Map<String, Event> events = new HashMap<>();
+    private final EventRepository eventRepository;
+    private final UserRepository userRepository;
 
-    public Collection<Event> getEvents(){
-        return events.values();
+    public EventService(EventRepository eventRepository, UserRepository userRepository){
+        this.eventRepository = eventRepository;
+        this.userRepository = userRepository;
     }
 
-    public void addEvent(Event event){
-        events.put(event.getId(), event);
+
+    public List<EventResponse> getEvents(){
+        return eventRepository.findAll().stream().map(EventResponse::fromEvent).toList();
+    }
+
+    public void addEvent(EventRequest request){
+        User user = userRepository.findById(request.getUserId()).orElseThrow(() -> new RuntimeException("User not found"));
+        Event event = new Event();
+        event.setEventName(request.getEventName());
+        event.setLocation(request.getLocation());
+        event.setDate(request.getDate());
+        event.setPassword(request.getPassword());
+        event.setQrCode(request.getQrCode());
+        event.setUser(user);
+        eventRepository.save(event);
     }
 }
