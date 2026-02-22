84c5aa2fab8c8f25c19815b618ba5fedca3923ab
DomenicWalther <domenicwalthercreatiiv@gmail.com>
2026-02-22 21:25:13 +0100

    feat(auth): add Auth DTO's

---
+++ src/main/java/com/domenicwalther/brautcloud/dto/AuthRequest.java
@@ -0,0 +1,4 @@
+package com.domenicwalther.brautcloud.dto;
+
+public record AuthRequest(String email, String password) {
+}
---
+++ src/main/java/com/domenicwalther/brautcloud/dto/AuthResponse.java
@@ -0,0 +1,4 @@
+package com.domenicwalther.brautcloud.dto;
+
+public record AuthResponse(String token) {
+}
